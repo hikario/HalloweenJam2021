71764d20bc7473d2bdc25f4199e4fab96f19e56d
hikario <58058800+hikario@users.noreply.github.com>
2021-10-30 01:28:48 -0700

    Snapshot of progress

---
+++ Assets/Scripts/ButtonMethods.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonMethods : MonoBehaviour
+{
+    public GameObject poemPiece;
+    private CanvasGroup canvasGroup;
+
+    public GameObject collectible;
+
+    public SpriteRenderer giftRenderer;
+
+    public void ItemClick()
+    {
+        if (poemPiece.activeSelf == false)
+        {
+            poemPiece.SetActive(true);
+            canvasGroup = gameObject.GetComponentInParent<CanvasGroup>();
+            canvasGroup.interactable = false;
+        }
+    }
+
+    public void PageClick()
+    {
+
+    }
+
+    public void OpenPackage(string buttonName)
+    {
+        collectible.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/ItemsManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Fungus;
+
+public class ItemsManager : MonoBehaviour
+{
+    private Canvas canDesk;
+    private Canvas canPoem;
+
+    [SerializeField]
+    private Button journalItem;
+    [SerializeField]
+    private Button roseItem;
+    [SerializeField]
+    private Button knifeItem;
+    [SerializeField]
+    private Button mirrorItem;
+    [SerializeField]
+    private Button candleItem;
+
+// will fix l8r
+    // [SerializeField]
+    // private Button journalPoem;
+    // [SerializeField]
+    // private Button rosePoem;
+    // [SerializeField]
+    // private Button knifePoem;
+    // [SerializeField]
+    // private Button mirrorPoem;
+    // [SerializeField]
+    // private Button candlePoem;
+
+    void Start()
+    {
+        // Turning off everything
+    }
+
+    void GoToDesk()
+    {
+        // Turn off all Canvases
+        // Turn on Desk
+        // Turn on all acquired desk items
+    }
+
+    void GoToPoems()
+    {
+
+    }
+}
