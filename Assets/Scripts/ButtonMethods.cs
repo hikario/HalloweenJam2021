e61530ac295b0659bf34733fe9337037340d3b0d
Heather Wright <heatherwright@lcad.edu>
2021-10-30 05:09:27 -0700

    First gameplay loop functioning

--- Assets/Scripts/ButtonMethods.cs
+++ Assets/Scripts/ButtonMethods.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Fungus;
 
 public class ButtonMethods : MonoBehaviour
 {
+    public Button item;
     public GameObject poemPiece;
     private CanvasGroup canvasGroup;
 
-    public GameObject collectible;
-
-    public SpriteRenderer giftRenderer;
+    public int currentScene;
+    public int totalScenes;
 
     public void ItemClick()
     {
@@ -21,14 +24,24 @@ public class ButtonMethods : MonoBehaviour
         }
     }
 
-    public void PageClick()
+    public void NoteClick()
     {
-
+        if (poemPiece.activeSelf == true)
+        {
+            poemPiece.SetActive(false);
+            canvasGroup = gameObject.GetComponentInParent<CanvasGroup>();
+            canvasGroup.interactable = true;
+        }
     }
 
-    public void OpenPackage(string buttonName)
+    public void TurnPage()
     {
-        collectible.SetActive(true);
-        gameObject.SetActive(false);
+        currentScene++;
+        Fungus.Flowchart.BroadcastFungusMessage("Start Scene " + currentScene);
+
+        if (currentScene >= 7)
+        {
+            currentScene = 0;
+        }
     }
 }
